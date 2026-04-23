71d6661f32f0d613e37760a5d7cd6afd0da817d9
Brayden Youngberg <bjyberg1@gmail.com>
2026-04-23 10:52:03 +0200

    feat(sitemap and llm access): Add sitemap and data.org schema to make more LLM and SEO friendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "static",
   site: "https://bjyberg.github.io/CDH_Prototype/",
   base: "/CDH_Prototype",
+  integrations: [sitemap()],
   vite: {
     build: {
       rollupOptions: {
